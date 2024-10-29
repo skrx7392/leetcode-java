35fcbe69b8b64cb2c9516a39dce2e5e4bb2abebb
Sai Krishna Podduturi <skrx7392@gmail.com>
2024-10-29 01:47:53 -0700

    updated test

--- app/src/test/java/org/example/medium/_80RemoveDuplicatesFromSortedArray2Test.java
+++ app/src/test/java/org/example/medium/_80RemoveDuplicatesFromSortedArray2Test.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class _80RemoveDuplicatesFromSortedArray2Test {
 
     @ParameterizedTest
-    @CsvSource({"'[1,1,1,2,2,3]', 5", "'[0,0,1,1,1,1,2,3,3]', 7"})
+    @CsvSource({"'[1,1,1,2,2,3]', 5", "'[0,0,1,1,1,1,2,3,3]', 7", "[1], 1"})
     void removeDuplicates(String numsString, int expected) {
         _80RemoveDuplicatesFromSortedArray2 obj = new _80RemoveDuplicatesFromSortedArray2();
         var nums = convertStringToIntArray(numsString);
